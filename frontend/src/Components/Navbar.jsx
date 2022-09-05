c63a51f4c00e0ec141a703c59b1c39f2981d8e58
Andre Lim <Andre_lim99@outlook.com>
2022-09-05 21:29:13 +0800

    feat: Added a default.css for storing our colour palette

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Styles/Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="wrapper">Navbar</div>;
 };
 
 export default Navbar;

0edebdf0f7d8cd21f56a06cc2554aec45b3f6c10
Andre Lim <Andre_Lim99@outlook.com>
2022-09-11 19:45:18 +0800

    feat: divider component

---
+++ frontend/src/Components/Divider.jsx
@@ -0,0 +1,21 @@
+import { Container } from "@mui/system";
+import React from "react";
+import colors from "./colors";
+
+const Divider = () => {
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        width: "inherit",
+        bgcolor: "#F1EDED",
+        height: "5px",
+        borderRadius: "5px",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        margin: "20px",
+      }}
+    />
+  );
+};
+
+export default Divider;

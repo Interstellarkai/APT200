d42ae0da6927d00feb6ff53d35a27cb0030bc8e6
Andre Lim <Andre_Lim99@outlook.com>
2022-09-11 14:14:22 +0800

    feat: Added MerchantDice's logos

---
+++ frontend/src/Components/MerchantDiceLogo.jsx
@@ -0,0 +1,11 @@
+import { SvgIcon } from "@mui/material";
+import React from "react";
+
+import { ReactComponent as Logo } from "../Assets/logo.svg";
+// import logo from "../Assets/logo.svg";
+
+const MerchantDiceLogo = () => {
+  return <SvgIcon component={Logo} inheritViewBox />;
+};
+
+export default MerchantDiceLogo;
---
+++ frontend/src/Components/colors.jsx
@@ -0,0 +1,6 @@
+const colors = {
+  mainBlue: "#1b5edd",
+  mainGrey: "#4b5563",
+};
+
+export default colors;

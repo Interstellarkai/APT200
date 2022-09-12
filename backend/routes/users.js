411df7861bf708e7e7a230f73e71ebffd86b0e29
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-09-12 22:53:29 +0800

    feat: testing modules

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,71 +1,72 @@
-const bcrypt = require('bcrypt')
-const usersRouter = require('express').Router()
-const User = require('../models/user')
+const bcrypt = require("bcrypt");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('products')
-  response.json(users)
-})
+usersRouter.get("/", async (request, response) => {
+	const users = await User.find({}).populate("products");
+	response.json(users);
+});
 
 //usersRouter.get('/
 
-usersRouter.get('/:id', async (request, response) => {
-  const user = await User.findById(request.params.id)
-  if (user) {
-    response.json(user)
-  } else {
-    response.status(404).end()
-  }
-})
+usersRouter.get("/:id", async (request, response) => {
+	const user = await User.findById(request.params.id);
+	if (user) {
+		response.json(user);
+	} else {
+		response.status(404).end();
+	}
+});
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.post("/", async (request, response) => {
+	const { username, name, password } = request.body;
 
-  const existingUser = await User.findOne({ username })
-  if (existingUser) {
-    return response.status(400).json({
-      error: 'username must be unique'
-    })
-  }
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+	const existingUser = await User.findOne({ username });
+	if (existingUser) {
+		return response.status(400).json({
+			error: "username must be unique",
+		});
+	}
+	const saltRounds = 10;
+	const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+	const user = new User({
+		username,
+		name,
+		passwordHash,
+	});
 
-  const savedUser = await user.save()
+	const savedUser = await user.save();
 
-  response.status(201).json(savedUser)
-})
+	response.status(201).json(savedUser);
+});
 
-usersRouter.delete('/:id', async (request, response, next) => {
-  await User.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
+usersRouter.delete("/:id", async (request, response, next) => {
+	await User.findByIdAndRemove(request.params.id);
+	response.status(204).end();
+});
 
-usersRouter.put('/:id', async(request, response, next) => {
-  
-  const {name,password} = request.body
-  if(name)  {
-    User.findByIdAndUpdate(request.params.id, {name:name}, { new: true })
-    .then(updatedUser => {
-      return response.json(updatedUser)
-    }).catch(error => next(error))
-  }
+usersRouter.put("/:id", async (request, response, next) => {
+	const { name, password } = request.body;
+	if (name) {
+		User.findByIdAndUpdate(request.params.id, { name: name }, { new: true })
+			.then((updatedUser) => {
+				return response.json(updatedUser);
+			})
+			.catch((error) => next(error));
+	} else {
+		const saltRounds = 10;
+		const passwordHash = await bcrypt.hash(password, saltRounds);
+		User.findByIdAndUpdate(
+			request.params.id,
+			{ passwordHash: passwordHash },
+			{ new: true }
+		)
+			.then((updatedUser) => {
+				return response.json(updatedUser);
+			})
+			.catch((error) => next(error));
+	}
+});
 
-  else{
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-   User.findByIdAndUpdate(request.params.id, {passwordHash:passwordHash}, { new: true })
-    .then(updatedUser => {
-      return response.json(updatedUser)
-    }).catch(error => next(error))
-  }
- 
-})
-
-
-module.exports = usersRouter
+module.exports = usersRouter;
---
+++ backend/test/temp.test.js
@@ -0,0 +1,3 @@
+test("properly adds two numbers", () => {
+	expect(sum(1, 2)).toBe(3);
+});

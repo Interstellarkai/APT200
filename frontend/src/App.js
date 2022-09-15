63784a1c229f7e78ce57194bf8c5728772f1f0fb
KaiSheng Lim <limkaisheng.work@gmail.com>
2022-09-15 16:37:58 +0800

    feat: component advertisement with timer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,81 +1,45 @@
-import Products from "./Components/Products";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
+// import Products from "./Components/Products";
+// import Home from "./Pages/Home";
+// import Login from "./Pages/Login";
+import Advertisement from "./Components/Advertisement";
 import colors from "./Components/colors";
 
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/material";
 
-let products = [
-  {
-    id: 1,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-  {
-    id: 2,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-  {
-    id: 3,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-  {
-    id: 4,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-  {
-    id: 5,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-  {
-    id: 6,
-    name: "iPhone 12",
-    price: 999,
-    description: "The latest iPhone",
-    media: "https://cdn.mos.cms.futurecdn.net/nCLvAtSHdMfMBHUtmu6TCf.jpg",
-  },
-];
-
 let customTheme = createTheme({
-  palette: {
-    primary: {
-      main: `${colors.mainBlue}`,
-    },
-    secondary: {
-      main: `${colors.mainGrey}`,
-    },
-  },
-  typography: {
-    allVariants: {
-      textTransform: "none",
-      fontFamily: `"Lexend Deca", sans-serif`,
-    },
-  },
+	palette: {
+		primary: {
+			main: `${colors.mainBlue}`,
+		},
+		secondary: {
+			main: `${colors.mainGrey}`,
+		},
+	},
+	typography: {
+		allVariants: {
+			textTransform: "none",
+			fontFamily: `"Lexend Deca", sans-serif`,
+		},
+	},
 });
 
 customTheme = responsiveFontSizes(customTheme);
 
+let product = {
+	id: 1,
+	name: "Jacket",
+	price: 99,
+	description: "Jacket",
+	media: "https://raw.githubusercontent.com/Interstellarkai/APT200/8cbc4e8870f3b88666cb75ebc6ae12e8ef7066a0/frontend/src/Assets/Item/jacket.png",
+};
+
 const App = () => {
-  return (
-    <ThemeProvider theme={customTheme}>
-      <Login />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={customTheme}>
+			<Advertisement product={product}/>
+		</ThemeProvider>
+	);
 };
 
 export default App;

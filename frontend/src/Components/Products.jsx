6e3cc09cab73f0ced12f9b747cd905220e87cc94
Andrew Ng <andrewngyongkuan@gmail.com>
2022-10-17 15:43:11 +0800

    Modify: Catalogue page

--- frontend/src/Components/Products.jsx
+++ frontend/src/Components/Products.jsx
@@ -2,40 +2,32 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Product from "./Product";
 
-// // Redux
-// import { useDispatch, useSelector } from "react-redux";
-// import { addProduct } from "../Redux/tmpProductSlice";
-
 const Products = ({ products }) => {
-  // Redux
-  // const curProducts = useSelector((state) => state.tmpProducts.value);
-  // const dispatch = useDispatch();
-
   if (!products.length) return <p>Loading...</p>;
 
   return (
-    <main
-      flexGrow="1"
-      backgroundColor="theme.palette.background.default"
-      padding="theme.spacing(3)"
+    // <main
+    //   // flexGrow="1"
+    //   backgroundColor="theme.palette.background.default"
+    //   // padding="theme.spacing(3)"
+    // >
+    //   {/* <div theme.mixins.toolbar />{" "} */}
+    <Grid
+      container
+      display={"flex"}
+      justifyContent={"center"}
+      spacing={4}
+      xs={12}
+      lg={11}
+      sx={{}}
     >
-      {/* <div theme.mixins.toolbar />{" "} */}
-      <Grid
-        container
-        display={"flex"}
-        justifyContent={"center"}
-        spacing={4}
-        xs={12}
-        lg={11}
-        sx={{}}
-      >
-        {products.map((product) => (
-          <Grid key={product.key} item xs={12} sm={10} md={8} lg={6} sx={{}}>
-            <Product product={product} />
-          </Grid>
-        ))}
-      </Grid>
-    </main>
+      {products.map((product) => (
+        <Grid key={product.key} item xs={12} sm={10} md={8} lg={6} sx={{}}>
+          <Product product={product} />
+        </Grid>
+      ))}
+    </Grid>
+    // </main>
   );
 };
 
